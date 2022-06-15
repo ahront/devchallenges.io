a55f7401f73e4ccaf3e97515ccca532dff27706a
Ahron Townsley <ahrontownsley@Ahrons-MacBook-Pro.local>
2022-06-15 16:21:29 +0300

    created different button types

--- front-end-challenges/button-component/src/App.js
+++ front-end-challenges/button-component/src/App.js
@@ -1,3 +1,5 @@
+
+import './App.css'
 import Button from './Components/Button'
 
 const App = () => {
@@ -6,8 +8,86 @@ const App = () => {
   const disabled = true
   return (
     <div>
-      <Button variant="" text="default" disableShadow/>
-      <Button variant="text" text="default" disabled/>
+      <h1>Buttons</h1>
+      <div className='container'>
+       
+        <h2>Button variants:</h2>
+          <div className='button-type'>
+            <div>
+              <p>{"<Button />"}</p>
+              <Button />
+            </div>
+            <div>
+              <p>{"<Button variant='outline' />"}</p>
+              <Button variant="outline" />
+            </div>
+            <div>
+              <p>{"<Button variant='text' />"}</p>
+              <Button variant="text" />
+            </div>
+          </div>
+          
+          <h2>Disable button shadow:</h2>
+          <div className='button-type'>
+            <div>
+              <p>{"<Button disableShadow />"}</p>
+              <Button disableShadow />
+            </div>
+          </div>
+
+          <h2>Disable button:</h2>
+          <div className="button-type">
+            <div>
+                <p>{"<Button disabled />"}</p>
+                <Button disabled />
+            </div>
+            <div>
+                <p>{"<Button variant='text' disabled />"}</p>
+                <Button variant="text" disabled />
+            </div>
+           </div>
+
+          <h2>Buttons with icons:</h2>
+          <div className='button-type'>
+            
+          </div>
+          
+          <h2>Button sizes:</h2>
+          <div className='button-type'>
+            <div>
+              <p>{"<Button size='sm' />"}</p>
+              <Button size="sm" />
+            </div>
+            <div>
+              <p>{"<Button size='md' />"}</p>
+              <Button size="md" />
+            </div>
+            <div>
+              <p>{"<Button size='lg' />"}</p>
+              <Button size="lg" />
+            </div>
+          </div>
+        
+        <h2>Button colors:</h2>
+        <div className='button-type'>
+          <div>
+            <p>{"<Button color='default' />"}</p>
+            <Button color="default" />
+          </div>
+          <div>
+            <p>{"<Button color='primary' />"}</p>
+            <Button color="primary" />
+          </div>
+          <div>
+            <p>{"<Button color='secondary' />"}</p>
+            <Button color="secondary" />
+          </div>
+          <div>
+            <p>{"<Button color='danger' />"}</p>
+            <Button color="danger" />
+          </div>
+        </div>
+      </div>
     </div>
   )
   
--- front-end-challenges/button-component/src/Components/Button.js
+++ front-end-challenges/button-component/src/Components/Button.js
@@ -1,12 +1,16 @@
-import '../App.css'
+import '../Button.css'
 
 const Button = props => {
-    const {text, variant, disableShadow, disabled} = props
+    const {text="Default", variant=null, disableShadow=null, disabled=null, size=null, color=null} = props
     return (
         <button className=
-            {`default ${variant} 
+            {`default 
+            ${variant && variant} 
             ${disableShadow && "disableShadow"} 
-            ${disabled && 'disabled'}`}>
+            ${disabled && 'disabled'}
+            ${size === 'sm' ? 'sm' : size === 'md' ? 'md' : size === "lg" ? 'lg' : ""}
+            ${color && `${color}-color`}`
+            } >
             {text}
         </button>
     )
